33e05b0c6419b99fb340006f40217da02f6c6fe2
EdmilsonEMAndrade <edmilson.andrad@gmail.com>
2022-05-06 11:47:26 -0300

    WATSON WEBCHAT identityToken

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+
+
+import { createRoot } from "react-dom/client";
+
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
